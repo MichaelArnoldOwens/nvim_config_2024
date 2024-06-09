156e78409a4601ef4b759f8171b2fe1901201a69
Michael Arnold Owens <anpganpg@gmail.com>
2024-06-08 22:36:17 -0700

    lsp show diagnostics in hover window

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -1,2 +1,9 @@
--- local lspconfig = require('lspconfig')
--- lspconfig.pylsp.setup {}
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 150
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+	group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+	callback = function()
+		vim.diagnostic.open_float(nil, { focus = false })
+	end,
+})
